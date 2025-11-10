51b264ae339522831d590f459ebe1860bef5e0c6
nemuineru <suteado16x16@gmail.com>
2025-11-10 23:41:21 +0900

    Speedtesting the PuerTS

--- Assets/MyAsset/PuerTSTest.cs
+++ Assets/MyAsset/PuerTSTest.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using Puerts.TSLoader;
 using Puerts;
+using System;
 
 public class PuerTSTest : MonoBehaviour
 {
+    public TMP_Text text;
+    public Action act;
+    Puerts.JsEnv env;
     //1. Hello World
     void Start() {
-        Puerts.JsEnv env = new Puerts.JsEnv();
-        env.Eval(@"
-            console.log('hello world');
-        ");
+        env = new Puerts.JsEnv();
     }
 
     // Update is called once per frame
     void Update()
     {
+        env.Eval(@"
+            //console.log('hello world');            
+        ");
         
     }
 }
